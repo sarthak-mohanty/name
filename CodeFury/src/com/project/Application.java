09ce0790ff073cbb0207948f5d2998b2f8c00b6b
sarthak-mohanty <sarthak0198@gmail.com>
2020-10-07 20:01:53 +0530

    ascasc

--- CodeFury/src/com/project/Application.java
+++ CodeFury/src/com/project/Application.java
@@ -7,6 +7,7 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //   System.out.println(ConnectionUtility.getDerbyConnection());
+		System.out.println("What the hell");
 	}
 
 }
